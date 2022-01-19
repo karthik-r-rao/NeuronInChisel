6d0d4ec4f46e8fc9075a0db9e2f16415b49a0d73
Karthik Rao <karthikrrao30@gmail.com>
2022-01-19 21:13:16 +0530

    change interface

--- src/main/scala/mac.scala
+++ src/main/scala/mac.scala
@@ -2,51 +2,56 @@ package neuron
 
 import chisel3._
 
-class DataInterface(dataWidth: Int) extends Bundle{
+class MultiplyAccumulateInterfaceIn(dataWidth: Int) extends Bundle{
+    val reset = Input(Bool())
+    
+    // data signals
     val bias_in = Input(Bits(dataWidth.W))
     val weight_in = Input(Bits(dataWidth.W))
     val data_in = Input(Bits(dataWidth.W))
-    val data_out = Output(Bits(dataWidth.W))
-}
 
-class ControlInterface extends Bundle{
-    val reset = Input(Bool())
+    // control signals
     val bias_in_valid = Input(Bool())
     val weight_in_valid = Input(Bool())
     val data_in_valid = Input(Bool())
+}
+
+class MultiplyAccumulateInterfaceOut(dataWidth: Int) extends Bundle{
+    // data signals
+    val data_out = Output(Bits(dataWidth.W))
+
+    // control signals
     val data_out_valid = Output(Bool())
 }
 
-class MultiplyAccumulateInterface(dataWidth: Int) extends Bundle{
-    val data_io = new DataInterface(dataWidth)
-    val control_io = new ControlInterface()
-}
-
 class MultiplyAccumulate(dataWidth: Int) extends Module{
-    val io = IO(new MultiplyAccumulateInterface(dataWidth))
+    val io = IO(new Bundle{
+        val mac_in = new MultiplyAccumulateInterfaceIn(dataWidth)
+        val mac_out = new MultiplyAccumulateInterfaceOut(dataWidth)
+    })
     val multiply = WireDefault(0.U(dataWidth.W))
     val input_valid = Wire(Bool())
     val acc = RegInit(0.U(dataWidth.W))
     val acc_valid = RegInit(false.B)
 
-    input_valid := io.control_io.weight_in_valid & io.control_io.data_in_valid
+    input_valid := io.mac_in.weight_in_valid & io.mac_in.data_in_valid
 
-    when (io.control_io.reset){
+    when (io.mac_in.reset){
         acc := 0.U
     }
 
     when (input_valid){
-        multiply := io.data_io.weight_in * io.data_io.data_in
+        multiply := io.mac_in.weight_in * io.mac_in.data_in
         acc := acc + multiply
     }
 
-    when (io.control_io.bias_in_valid){
-        acc := acc + io.data_io.bias_in
+    when (io.mac_in.bias_in_valid){
+        acc := acc + io.mac_in.bias_in
         acc_valid := true.B
     }
 
-    io.data_io.data_out := acc
-    io.control_io.data_out_valid := acc_valid
+    io.mac_out.data_out := acc
+    io.mac_out.data_out_valid := acc_valid
 }
 
 object DriverMultiplyAccumulate extends App{
--- src/test/scala/macTests.scala
+++ src/test/scala/macTests.scala
@@ -9,31 +9,31 @@ import neuron._
 class MultiplyAccumulateSpec extends AnyFlatSpec with ChiselScalatestTester{
     "Tester" should "pass" in{
         test(new MultiplyAccumulate(10)) { dut => 
-            dut.io.control_io.reset.poke(true.B)
-            dut.io.control_io.weight_in_valid.poke(false.B)
-            dut.io.control_io.data_in_valid.poke(false.B)
-            dut.io.control_io.bias_in_valid.poke(false.B)
+            dut.io.mac_in.reset.poke(true.B)
+            dut.io.mac_in.weight_in_valid.poke(false.B)
+            dut.io.mac_in.data_in_valid.poke(false.B)
+            dut.io.mac_in.bias_in_valid.poke(false.B)
             
             dut.clock.step()
 
             val weight_in = 15
             val data_in = 27
-            dut.io.control_io.weight_in_valid.poke(true.B)
-            dut.io.control_io.data_in_valid.poke(true.B)
-            dut.io.data_io.weight_in.poke(weight_in.U)
-            dut.io.data_io.data_in.poke(data_in.U)
+            dut.io.mac_in.weight_in_valid.poke(true.B)
+            dut.io.mac_in.data_in_valid.poke(true.B)
+            dut.io.mac_in.weight_in.poke(weight_in.U)
+            dut.io.mac_in.data_in.poke(data_in.U)
 
             dut.clock.step()
 
             val bias_in = 21
-            dut.io.control_io.bias_in_valid.poke(true.B)
-            dut.io.control_io.weight_in_valid.poke(false.B)
-            dut.io.control_io.data_in_valid.poke(false.B)
-            dut.io.data_io.bias_in.poke(bias_in.U)
+            dut.io.mac_in.bias_in_valid.poke(true.B)
+            dut.io.mac_in.weight_in_valid.poke(false.B)
+            dut.io.mac_in.data_in_valid.poke(false.B)
+            dut.io.mac_in.bias_in.poke(bias_in.U)
 
             dut.clock.step()
 
-            dut.io.data_io.data_out.expect((weight_in * data_in + bias_in).U)
+            dut.io.mac_out.data_out.expect((weight_in * data_in + bias_in).U)
         }
     }
 }
