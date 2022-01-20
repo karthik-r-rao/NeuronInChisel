b24f45ab7c987cbdb2ffa3b89c337f356536e98e
Karthik Rao <karthikrrao30@gmail.com>
2022-01-20 11:20:12 +0530

    add simple neuron test

--- src/test/scala/neuronTests.scala
+++ src/test/scala/neuronTests.scala
@@ -5,40 +5,58 @@ import chiseltest._
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 import chiseltest.simulator.WriteVcdAnnotation
+import scala.util.Random
+import scala.io.Source
 import neuron._ 
 
 class NeuronSpec extends AnyFlatSpec with ChiselScalatestTester{
     "Tester" should "pass" in{
         test(new Neuron(1024, 10)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
-            dut.io.neuron_in.reset.poke(true.B)
-            dut.io.neuron_in.weight_valid.poke(false.B)
-            dut.io.neuron_in.data_valid.poke(false.B)
-            dut.io.neuron_in.bias_valid.poke(false.B)
-            
-            dut.clock.step()
+            val lines = Source.fromResource("lut.txt").getLines().toArray
+            for (i <- 0 until 1023){
+                dut.io.neuron_in.reset.poke(true.B)
+                dut.io.neuron_in.weight_valid.poke(false.B)
+                dut.io.neuron_in.data_valid.poke(false.B)
+                dut.io.neuron_in.bias_valid.poke(false.B)
+                
+                dut.clock.step()
 
-            val weight_in = 9
-            val data_in = 111
-            dut.io.neuron_in.weight_valid.poke(true.B)
-            dut.io.neuron_in.data_valid.poke(true.B)
-            dut.io.neuron_in.weight.poke(weight_in.U)
-            dut.io.neuron_in.data.poke(data_in.U)
+                var acc = 0
+                var weight_in = Random.between(0, 16)
+                var data_in = Random.between(0, 16)
+                acc = acc + weight_in * data_in
+                dut.io.neuron_in.weight_valid.poke(true.B)
+                dut.io.neuron_in.data_valid.poke(true.B)
+                dut.io.neuron_in.weight.poke(weight_in.U)
+                dut.io.neuron_in.data.poke(data_in.U)
 
-            dut.clock.step()
+                dut.clock.step()
 
-            val bias_in = 1
-            dut.io.neuron_in.bias_valid.poke(true.B)
-            dut.io.neuron_in.weight_valid.poke(false.B)
-            dut.io.neuron_in.data_valid.poke(false.B)
-            dut.io.neuron_in.bias.poke(bias_in.U)
+                weight_in = Random.between(0, 16)
+                data_in = Random.between(0, 16)
+                acc = acc + weight_in * data_in
+                dut.io.neuron_in.weight_valid.poke(true.B)
+                dut.io.neuron_in.data_valid.poke(true.B)
+                dut.io.neuron_in.weight.poke(weight_in.U)
+                dut.io.neuron_in.data.poke(data_in.U)
 
-            dut.clock.step()
+                dut.clock.step()
 
-            dut.io.neuron_in.bias_valid.poke(false.B)
+                var bias_in = Random.between(0, 16)
+                acc = acc + bias_in
+                dut.io.neuron_in.bias_valid.poke(true.B)
+                dut.io.neuron_in.weight_valid.poke(false.B)
+                dut.io.neuron_in.data_valid.poke(false.B)
+                dut.io.neuron_in.bias.poke(bias_in.U)
 
-            dut.clock.step()
+                dut.clock.step()
 
-            dut.io.neuron_out.data.expect(("b" + "0000000011").U)
+                dut.io.neuron_in.bias_valid.poke(false.B)
+
+                dut.clock.step()
+
+                dut.io.neuron_out.data.expect(("b" + lines(acc)).U)
+            }
         }
     }
 }
