e1f3c5a4af5dc087cb0de632cac601c12bf0e880
Karthik Rao <karthikrrao30@gmail.com>
2022-01-27 23:30:11 +0530

    add comment

--- src/main/scala/header.scala
+++ src/main/scala/header.scala
@@ -1,7 +1,10 @@
+// include project-wide stuff here
+
 package nn
 
 import chisel3._
 
+// bundle to be used for data signals throughout the project
 class NNWire(width: Int) extends Bundle{
     val data = Bits(width.W)
     val valid = Bool()
